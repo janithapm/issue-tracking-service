2d2a836ac5fb46c1b7f19433e351929af760ac88
Janitha Madushanka <janitha.madushanka@circles.co>
2024-07-15 18:56:11 +0200

    feature: updated input dto validation

--- src/due-dates/DueDates.dto.ts
+++ src/due-dates/DueDates.dto.ts
@@ -1,17 +1,20 @@
-import { IsDateString, IsNotEmpty, Min } from "class-validator"
+import { IsDateString, IsNotEmpty, IsNumber, IsString, Matches, Min } from "class-validator"
 
 export class StartDateDTO {
-    @IsNotEmpty()
-    @IsDateString()
+
+    @IsNotEmpty() @IsDateString() @IsString()
+    @Matches(/^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01])$/, { message: 'Allowed date formats: (YYYY-MM-DD) or (YYYY/MM/DD)' })
     startDate: string
-    @IsNotEmpty()
+
+    @IsNotEmpty() @IsString()
+    @Matches(/^([0-1]?[0-9]|2[0-3]):([0-5][0-9])(?: (?:am|pm))?$/i, { message: 'Invalid time format (HH:MM (am/pm)?)' })
     startTime: string
-    @IsNotEmpty()
-    @Min(0)
-    turnAroundTime: Number
+
+    @IsNotEmpty() @IsNumber() @Min(0)
+    turnAroundTime: number
 }
 
 export interface StartDate {
     start: Date,
-    turnAroundTime: Number
+    turnAroundTime: number
 }
