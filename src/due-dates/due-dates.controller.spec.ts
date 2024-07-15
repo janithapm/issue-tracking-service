a48249cef082d7c4eef79c1051e3b44b51aeb1ef
Janitha Madushanka <janitha.madushanka@circles.co>
2024-07-15 07:51:25 +0200

    feature: initial test cases

--- src/due-dates/due-dates.controller.spec.ts
+++ src/due-dates/due-dates.controller.spec.ts
@@ -1,12 +1,23 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { DueDatesController } from './due-dates.controller';
+import { DueDatesService } from './due-dates.service';
+
 
 describe('DueDatesController', () => {
   let controller: DueDatesController;
 
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [DueDatesController],
+      providers: [
+        {
+          provide: DueDatesService,
+          useValue: {
+            getDueDate: jest.fn().mockReturnValue('Mocked Due Date'), // Mock a method
+          },
+        },
+      ],
     }).compile();
 
     controller = module.get<DueDatesController>(DueDatesController);
