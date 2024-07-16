8cb71b73aee9bf26c36ccb0231b44808b33e75a6
Janitha Madushanka <janitha.madushanka@circles.co>
2024-07-16 21:29:23 +0200

    fix: change uri format

--- src/due-dates/due-dates.controller.ts
+++ src/due-dates/due-dates.controller.ts
@@ -1,15 +1,15 @@
-import { Body, Controller, Post, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, Get, UsePipes, ValidationPipe } from '@nestjs/common';
 import { DueDatesService } from './due-dates.service';
 import { StartDateDTO } from './DueDates.dto';
 
-@Controller('due-dates')
+@Controller('dueDate')
 export class DueDatesController {
 
     constructor(private dueDateService: DueDatesService) {
 
     }
 
-    @Post()
+    @Get()
     @UsePipes(ValidationPipe)
     getDueDate(@Body() param: StartDateDTO){
         return this.dueDateService.getDueDate(param);
